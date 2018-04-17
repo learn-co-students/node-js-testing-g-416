211f718df932ee9a2b9372d11babc5d8504ccac0
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-17 16:49:20 +0200

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,50 @@
-const chai = require('chai')
-const supertest = require('supertest')
+const chai = require('chai');
+const supertest = require('supertest');
 /* ADD ME! */
-const app = require('../app')
+const app = require('../app');
 
-const expect = chai.expect
+const expect = chai.expect;
 
-describe('app', () => {
-  it('runs')
-})
+let server;
+
+before(function(done) {
+  return app.up().then(_server => {
+    server = _server;
+    done()
+  })
+});
+
+after(function() {
+  server.close()
+});
+
+describe('app', function() {
+  describe('up', function() {
+    it('is a function', function() {
+      expect(app.up).to.be.an.instanceof(Function)
+    })
+  });
+
+  describe('/user', function() {
+    describe('POST', function() {
+      it('fails with an empty request body', function(done) {
+        supertest(server).
+        post('/user').
+        expect(400, done)
+      });
+
+      /** This is new! */
+      it('succeeds with valid name, username, and email', function(done) {
+        supertest(server).
+        post('/user').
+        send({
+          email: 'test@email.com',
+          name: 'testName',
+          username: 'testUsername'
+        }).
+        set('content-type', 'application/json').
+        expect(200, done)
+      })
+    })
+  });
+});
--- test/models/user-test.js
+++ test/models/user-test.js
@@ -1,12 +1,28 @@
-const chai = require('chai')
+const chai = require('chai');
+const supertest = require('supertest');
 
-const bookshelf = require('../../app/db/bookshelf')
-const User = require('../../app/models/user')
+const bookshelf = require('../../app/db/bookshelf');
+const User = require('../../app/models/user');
 
-const expect = chai.expect
+const expect = chai.expect;
 
 const mockUser = {
   email: 'email@email.com',
   name: 'Name',
   username: 'username'
-}
+};
+
+describe('User', function() {
+  let transaction;
+
+  beforeEach(done => {
+    bookshelf.transaction(t => {
+      transaction = t;
+      done();
+    })
+  });
+
+  afterEach(function(){
+    return transaction.rollback();
+  });
+});
