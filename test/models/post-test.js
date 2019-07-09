a8387350e233f7f24a89cb75414e7721bffdb276
Pat  Pignataro <pat6284@msn.com>
2019-07-09 17:24:39 +0000

    Done.

---
+++ test/models/post-test.js
@@ -0,0 +1,36 @@
+"use strict"
+
+ const chai = require('chai')
+
+ const bookshelf = require('../../app/db/bookshelf')
+const Post = require('../../app/models/post')
+
+ const expect = chai.expect
+
+ const mockPost = {
+  author: 'Alfonzo'
+}
+
+ describe('Post', () => {
+  let transaction;
+
+   beforeEach(done => {
+    return bookshelf.transaction(t => {
+      transaction = t
+      done()
+    })
+  })
+
+   afterEach(() => {
+    transaction.rollback()
+  })
+
+   it('saves a Post to the database', () => {
+    return Post.forge()
+      .save(mockPost, { transacting: transaction})
+      .then(post => {
+        expect(post.get('id')).to.be.a('number')
+      })
+  })
+
+ })
