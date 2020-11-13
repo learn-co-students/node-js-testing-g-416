fed692943c7aec6b47cc4fe03e68bcd0b9b472be
markdenardo <mark.denardo@gmail.com>
2020-11-13 16:58:56 -0500

    solution

--- test/models/user-test.js
+++ test/models/user-test.js
@@ -1,12 +1,49 @@
 const chai = require('chai')
-
-const bookshelf = require('../../app/db/bookshelf')
-const User = require('../../app/models/user')
+const supertest = require('supertest')
+/* ADD ME! */
+const app = require('../app')
 
 const expect = chai.expect
 
-const mockUser = {
-  email: 'email@email.com',
-  name: 'Name',
-  username: 'username'
-}
+let server
+
+before(done => {
+  return app.up().then(_server => {
+    server = _server
+    done()
+  })
+})
+
+after(() => {
+  server.close()
+})
+
+describe('app', () => {
+  describe('up', () => {
+    it('is a function', () => {
+      expect(app.up).to.be.an.instanceof(Function)
+    })
+  })
+
+  describe('/user', () => {
+    describe('POST', () => {
+      it('fails with an empty request body', done => {
+        supertest(server).
+          post('/user').
+          expect(400, done)
+      })
+
+      it('succeeds with valid name, username, and email', done => {
+        supertest(server).
+          post('/user').
+          send({
+            email: 'test@email.com',
+            name: 'testName',
+            username: 'testUsername'
+          }).
+          set('content-type', 'application/json').
+          expect(200, done)
+      })
+    })
+  })
+})
